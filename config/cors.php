735fddab1f15bf1e12e0563ed8e369fe8a59dfc8
Dinuka225 <dinukasaranga25@gmail.com>
2026-05-28 09:17:34 +0530

    fixing login issues

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], 
+    'allowed_origins' => ['http://localhost:3000','http://localhost:3001','https://raging-fire-e-store-fe.vercel.app','https://sme-management-system-frontend.vercel.app'], 
 
     'allowed_origins_patterns' => [],
 
